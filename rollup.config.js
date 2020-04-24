e5f8f9be5ae1705499228e3dd3628ba42e79a075
Marco Starker <marco.starker@gmx.net>
2020-04-24 13:20:02 +0200

    dep update

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,100 @@
-import resolve from 'rollup-plugin-node-resolve';
-import progress from 'rollup-plugin-progress';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import {terser} from "rollup-plugin-terser";
 import clear from "rollup-plugin-clear";
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
+
+const babelOptionsES5 = {
+    runtimeHelpers: false,
+    externalHelpers: false,
+    babelrc: false,
+    presets: [
+        [
+            "@babel/env",
+            {
+                targets: {
+                    ie: '11',
+                    edge: '17',
+                    firefox: '60',
+                    chrome: '71',
+                    safari: '11.1',
+                },
+            }
+        ]
+    ],
+};
+
+const babelOptionsES2018 = {
+    runtimeHelpers: false,
+    externalHelpers: false,
+    babelrc: false,
+};
 
 // https://rollupjs.org/guide/en#big-list-of-options
 export default [
+    // ES2015 Minified
+    {
+        input: './src/eventEmitter.js',
+        output: {
+            file: './dist/eventEmitter.min.js',
+            format: 'iife',
+            name: 'EventEmitter',
+            compact: true,
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            clear({targets: ['./dist']}),
+            babel(babelOptionsES5),
+            terser(),
+        ]
+    },
+    // ES2015 None-Minified
+    {
+        input: './src/eventEmitter.js',
+        output: {
+            file: './dist/eventEmitter.js',
+            format: 'iife',
+            name: 'EventEmitter',
+            compact: false,
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            babel(babelOptionsES5),
+        ]
+    },
+    // ES Modules Minified
+    {
+        input: './src/eventEmitter.js',
+        output: {
+            file: './dist/eventEmitter.esm.min.js',
+            format: 'esm',
+            compact: true,
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            babel(babelOptionsES2018),
+            terser(),
+        ]
+    },
+    // ES Modules None-Minified
+    {
+        input: './src/eventEmitter.js',
+        output: {
+            file: './dist/eventEmitter.esm.js',
+            format: 'esm',
+            compact: false,
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(),
+            babel(babelOptionsES2018),
+        ]
+    },
     // Node Module
     {
         input:   './src/eventEmitter.js',
@@ -16,93 +104,8 @@ export default [
             name:   'EventEmitter',
         },
         plugins: [
-            clear({targets: ['./dist']}),
-            progress({clearLine: false}),
             resolve(),
             commonjs(),
-        ]
-    },
-    // ES2015 Minified
-    {
-        input:   './src/eventEmitter.js',
-        output:  {
-            file:      './dist/eventEmitter.min.js',
-            format:    'iife',
-            name:      'EventEmitter',
-            compact:   true,
-            sourcemap: true,
-        },
-        plugins: [
-            progress({clearLine: false}),
-            resolve(),
-            commonjs(),
-            babel({
-                runtimeHelpers:  false,
-                externalHelpers: false,
-                babelrc:         false,
-                ignore:          [/\/core-js\//],
-                presets:         [
-                    [
-                        '@babel/preset-env',
-                        {
-                            useBuiltIns: 'usage',
-                            corejs:      {
-                                version:   2,
-                                proposals: true
-                            },
-                            targets:     {
-                                ie:      '11',
-                                edge:    '17',
-                                firefox: '60',
-                                chrome:  '71',
-                                safari:  '11.1',
-                            },
-                        }
-                    ]
-                ],
-            }),
-            terser({sourcemap: true}),
-        ]
-    },
-    // ES2015 None-Minified
-    {
-        input:   './src/eventEmitter.js',
-        output:  {
-            file:      './dist/eventEmitter.js',
-            format:    'iife',
-            name:      'EventEmitter',
-            compact:   false,
-            sourcemap: true,
-        },
-        plugins: [
-            progress({clearLine: false}),
-            resolve(),
-            commonjs(),
-            babel({
-                runtimeHelpers:  false,
-                externalHelpers: false,
-                babelrc:         false,
-                ignore:          [/\/core-js\//],
-                presets:         [
-                    [
-                        '@babel/preset-env',
-                        {
-                            useBuiltIns: 'usage',
-                            corejs:      {
-                                version:   2,
-                                proposals: true
-                            },
-                            targets:     {
-                                ie:      '11',
-                                edge:    '17',
-                                firefox: '60',
-                                chrome:  '71',
-                                safari:  '11.1',
-                            },
-                        }
-                    ]
-                ],
-            }),
         ]
     },
 ];
